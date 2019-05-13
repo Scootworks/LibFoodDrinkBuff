5f294a4812df37d89fdc89ba6696675c20056f81
Baertram <pz1@gmx.net>
2019-05-13 19:36:08 +0200

    BETA v8
    Complete reworked library

---
+++ LibFoodDrinkBuff/lang/en.lua
@@ -0,0 +1,7 @@
+ZO_CreateStringId("SI_LIB_FOOD_DRINK_BUFF_EXCEL", "[<<1>>] = true, -- <<2>>")
+ZO_CreateStringId("SI_LIB_FOOD_DRINK_BUFF_ABILITY_NAME", "<<C:1>>")
+
+ZO_CreateStringId("SI_LIB_FOOD_DRINK_BUFF_RELOAD", "The user interface will be reloaded in |cFF00005 seconds|r!")
+ZO_CreateStringId("SI_LIB_FOOD_DRINK_BUFF_EXPORT_START", "Searching food / drinks has begun. This may take several seconds...")
+ZO_CreateStringId("SI_LIB_FOOD_DRINK_BUFF_EXPORT_FINISH", "The search is over. <<C:1[No/$d/$d]>> food / drinks were exported.")
+ZO_CreateStringId("SI_LIB_FOOD_DRINK_BUFF_ARGUMENT_MISSING", "<<1>>!\nArgument for |cFFFFFF/dumpfdb|r is missing!\nuse |cFFFFFFall|r - dumps the full list\nor |cFFFFFFnew|r - only dump new foods / drinks")
