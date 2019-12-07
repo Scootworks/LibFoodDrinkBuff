3f8eed115acae98dc3cea04711247dc23bd932b2
Marc <scootworks@me.com>
2019-12-07 13:40:37 +0100

    fr update

--- LibFoodDrinkBuff/lang/fr.lua
+++ LibFoodDrinkBuff/lang/fr.lua
@@ -4,4 +4,4 @@ SafeAddString(SI_LIB_FOOD_DRINK_BUFF_EXPORT_FINISH, "Le recherche est terminée.
 SafeAddString(SI_LIB_FOOD_DRINK_BUFF_ARGUMENT_MISSING, "<<1>>!\nParamètre manquant pour for |cFFFFFF/dumpfdb|r!\nUtilisez |cFFFFFFall|r - exporte la liste complète ou\n|cFFFFFFnew|r - exporte seulement la nourriture / boisson", 0)
 SafeAddString(SI_LIB_FOOD_DRINK_BUFF_NO_BUFFS, "Il n'y a pas de buff actif.", 0)
 
-SafeAddString(SI_LIB_FOOD_DRINK_BUFF_DIALOG_MAINTEXT, "<<1>> nourriture / boisson ont été trouvés.\n\nVous devez recharger l'interface utilisateur pour mettre à jour votre savedVariable enregistrées.\n\nVoulez-vous recharger l'interface utilisateur maintenant?", 0)
+SafeAddString(SI_LIB_FOOD_DRINK_BUFF_DIALOG_MAINTEXT, "<<1>> nourritures / boissons trouvé(s).\n\nVous devez recharger l'interface pour mettre à jour le fichier savedVariables.\n\nRecharger l'interface maintenant?", 0)
