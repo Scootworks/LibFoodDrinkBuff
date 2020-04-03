6f737a26b9df0f41f6096fe031ce468c82f8d4fe
Marc <scootworks@me.com>
2020-04-03 23:49:33 +0200

    removed double checked worldname

--- LibFoodDrinkBuff/LibFoodDrinkBuff_Collector.lua
+++ LibFoodDrinkBuff/LibFoodDrinkBuff_Collector.lua
@@ -48,16 +48,15 @@ function lib:InitializeCollector()
 
 			-- Get and set the SavedVariables. We are not using ZO_SavedVars wrapper here but just the global table of self.svName!
 			if not self.sv then
-				_G[self.svName] = _G[self.svName] or { }
+				_G[self.svName] = { }
 				self.sv = _G[self.svName]
 				--TODO: Why do we need this?
 				_G[self.svName] = self.sv
 			end
 			-- Clear old savedVars food and drink buff list of the current server
 			local sv = self.sv
-			local foodDrinkBuffList = sv.foodDrinkBuffList
-			foodDrinkBuffList = foodDrinkBuffList or {}
-			foodDrinkBuffList[worldName] = {}
+			sv.foodDrinkBuffList = sv.foodDrinkBuffList or { }
+			sv.foodDrinkBuffList[worldName] = { }
 
 			-- start new scan
 			startScanAbilities:For(1, MAX_ABILITY_ID):Do(function(abilityId)
@@ -66,8 +65,8 @@ function lib:InitializeCollector()
 				end
 			end):Then(function()
 				-- update the savedVars timestamp
-				sv.lastUpdated = sv.lastUpdated or {}
-				sv.lastUpdated[worldName] = {}
+				sv.lastUpdated = sv.lastUpdated or { }
+				sv.lastUpdated[worldName] = { }
 				local lastUpdated = sv.lastUpdated[worldName]
 				lastUpdated.timestamp = os.date()
 				lastUpdated.saveType = saveType
@@ -80,8 +79,6 @@ function lib:InitializeCollector()
 	end
 
 	function lib:NotificationAfterCreatingFoodDrinkTable(worldName)
-		worldName = worldName or GetWorldName()
-		if not self.sv or not self.sv.foodDrinkBuffList or not self.sv.foodDrinkBuffList[worldName] then return end
 		local countEntries = #self.sv.foodDrinkBuffList[worldName]
 		if countEntries > 0 then
 			local data = { countEntries = countEntries }
@@ -92,14 +89,10 @@ function lib:InitializeCollector()
 	end
 
 	function lib:AddToFoodDrinkTable(abilityId, saveType, worldName)
-		if not self.sv then return end
-		worldName = worldName or GetWorldName()
 		if saveType == ARGUMENT_NEW and self:GetBuffTypeInfos(abilityId) ~= NONE then
 			return
 		end
-		self.sv.foodDrinkBuffList = self.sv.foodDrinkBuffList or {}
-		self.sv.foodDrinkBuffList[worldName] = self.sv.foodDrinkBuffList[worldName] or {}
-		local foodDrinkBuffListOfWorldName = self.sv.foodDrinkBuffList[worldName]
+
 		-- We gonna check the abilityId parameter step by step to increase performance during the check.
 		if GetAbilityAngleDistance(abilityId) == 0 then
 			if GetAbilityRadius(abilityId) == 0 then
@@ -118,7 +111,7 @@ function lib:InitializeCollector()
 											ability.abilityId = abilityId
 											ability.abilityName = ZO_CachedStrFormat(SI_ABILITY_NAME, abilityName)
 											ability.lua = ZO_CachedStrFormat(SI_LIB_FOOD_DRINK_BUFF_EXCEL, abilityId, abilityName)
-											table.insert(foodDrinkBuffListOfWorldName, ability)
+											table.insert(self.sv.foodDrinkBuffList[worldName], ability)
 										end
 									end
 								end
